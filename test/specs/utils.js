ce06271b7b38038d22a89ad2a60643c20e941f95
Scott Doxey <neogeek0101@gmail.com>
2016-10-23 11:53:16 -0400

    Added test.
    Changed test descriptions.

--- test/specs/utils.js
+++ test/specs/utils.js
@@ -1,26 +1,32 @@
 const assert = require('assert');
 
-const findPackageFileInPath = require('../../lib/utils').findPackageFileInPath;
+const utils = require('../../lib/utils');
 
 describe('doxdox util cmd', () => {
 
     describe('findPackageFileInPath', () => {
 
-        it('findPackageFileInPath without input', () => {
+        it('find package without input', () => {
 
-            assert.deepEqual(findPackageFileInPath(), `${process.cwd()}/package.json`);
-
-        });
-
-        it('findPackageFileInPath with input directory', () => {
-
-            assert.deepEqual(findPackageFileInPath('./'), `${process.cwd()}/package.json`);
+            assert.deepEqual(utils.findPackageFileInPath(), `${process.cwd()}/package.json`);
 
         });
 
-        it('findPackageFileInPath with input file', () => {
+        it('find package with input directory', () => {
 
-            assert.deepEqual(findPackageFileInPath('./package.json'), `${process.cwd()}/package.json`);
+            assert.deepEqual(utils.findPackageFileInPath('./'), `${process.cwd()}/package.json`);
+
+        });
+
+        it('find package with input file', () => {
+
+            assert.deepEqual(utils.findPackageFileInPath('./package.json'), `${process.cwd()}/package.json`);
+
+        });
+
+        it('fail to find package with invalid directory', () => {
+
+            assert.deepEqual(utils.findPackageFileInPath('./testing'), null);
 
         });
 
