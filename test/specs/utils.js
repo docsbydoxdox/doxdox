482cd1799d68e090fb01e3660edef84817d08cfe
Scott Doxey <neogeek0101@gmail.com>
2016-10-24 22:14:52 -0400

    Changed assert method used.

--- test/specs/utils.js
+++ test/specs/utils.js
@@ -8,31 +8,31 @@ describe('doxdox utils', () => {
 
         it('find package without input', () => {
 
-            assert.deepEqual(utils.findPackageFileInPath(), `${process.cwd()}/package.json`);
+            assert.equal(utils.findPackageFileInPath(), `${process.cwd()}/package.json`);
 
         });
 
         it('find package with input directory', () => {
 
-            assert.deepEqual(utils.findPackageFileInPath('./'), `${process.cwd()}/package.json`);
+            assert.equal(utils.findPackageFileInPath('./'), `${process.cwd()}/package.json`);
 
         });
 
         it('find package with input file', () => {
 
-            assert.deepEqual(utils.findPackageFileInPath('./package.json'), `${process.cwd()}/package.json`);
+            assert.equal(utils.findPackageFileInPath('./package.json'), `${process.cwd()}/package.json`);
 
         });
 
         it('find package with input non package.json file', () => {
 
-            assert.deepEqual(utils.findPackageFileInPath('./index.js'), `${process.cwd()}/package.json`);
+            assert.equal(utils.findPackageFileInPath('./index.js'), `${process.cwd()}/package.json`);
 
         });
 
         it('fail to find package with invalid directory', () => {
 
-            assert.deepEqual(utils.findPackageFileInPath('./testing'), null);
+            assert.equal(utils.findPackageFileInPath('./testing'), null);
 
         });
 
