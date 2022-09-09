c8d3c59cd757002f7e9325963b9c1f51ce2f88b8
Scott Doxey <neogeek0101@gmail.com>
2022-09-09 14:51:08 -0400

    Switched to using snapshots in dox parser package.

--- packages/doxdox-parser-dox/src/index.test.ts
+++ packages/doxdox-parser-dox/src/index.test.ts
@@ -1,93 +1,19 @@
-import { parseString } from './index';
+import parse from './index';
 
 describe('dox parser', () => {
-    it('parse example jsdoc headers', async () => {
+    it('parse example jsdoc headers (declaration methods)', async () => {
         await expect(
-            parseString(
-                'lib/utils/index.js',
-                `/**
- * Finds file in path.
- *
- *     console.log(await findFileInPath('./', 'package.json'));
- *     console.log(await findFileInPath('../', 'package.json'));
- *     console.log(await findFileInPath('~/git/github/doxdox/', '.package.json'));
- *
- * @param {string} [input] Directory to check for file.
- * @param {string?} [fileName = 'package.json'] File name to check for.
- * @return {Promise<string | null>} Path to package.json file.
- * @public
- */
-
-const findFileInPath = async (input, fileName = 'package.json') => {};
-
-/**
- * Get the root directory of the package, supplied path or URL.
- *
- * @param {string?} [url] Optional path or URL.
- * @return {string} Directory path.
- * @public
- */
-
-const getRootDirPath = (url) => {};`
-            )
-        ).resolves.toEqual(
-            expect.objectContaining({
-                path: 'lib/utils/index.js',
-                methods: expect.arrayContaining([
-                    expect.objectContaining({
-                        type: 'declaration',
-                        fullName: 'findFileInPath(input, fileName)',
-                        name: 'findFileInPath',
-                        params: expect.arrayContaining([
-                            expect.objectContaining({
-                                name: 'input',
-                                types: ['string']
-                            }),
-                            expect.objectContaining({
-                                name: 'fileName',
-                                types: ['string']
-                            })
-                        ]),
-                        private: false,
-                        returns: expect.arrayContaining([
-                            expect.objectContaining({
-                                name: null,
-                                description: 'Path to package.json file.',
-                                types: ['Promise.<string|null>']
-                            })
-                        ]),
-                        slug: 'lib-utils-index-js-findfileinpath'
-                    }),
-                    expect.objectContaining({
-                        type: 'declaration',
-                        fullName: 'getRootDirPath(url)',
-                        name: 'getRootDirPath',
-                        params: expect.arrayContaining([
-                            expect.objectContaining({
-                                name: 'url',
-                                types: ['string']
-                            })
-                        ]),
-                        private: false,
-                        returns: expect.arrayContaining([
-                            expect.objectContaining({
-                                name: null,
-                                description: 'Directory path.',
-                                types: ['string']
-                            })
-                        ]),
-                        slug: 'lib-utils-index-js-getrootdirpath'
-                    })
-                ])
-            })
-        );
+            parse(process.cwd(), './test/mocks/declaration.js')
+        ).resolves.toMatchSnapshot();
+    });
+    it('parse example jsdoc headers (function methods)', async () => {
+        await expect(
+            parse(process.cwd(), './test/mocks/declaration.js')
+        ).resolves.toMatchSnapshot();
     });
     it('parse empty string', async () => {
-        await expect(parseString('test.js', '')).resolves.toEqual(
-            expect.objectContaining({
-                path: 'test.js',
-                methods: []
-            })
-        );
+        await expect(
+            parse(process.cwd(), './test/mocks/empty.js')
+        ).resolves.toMatchSnapshot();
     });
 });
