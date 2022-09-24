8720836827725f85f5d8bf228cb499847a24e6f1
Scott Doxey <neogeek0101@gmail.com>
2022-09-23 23:47:10 -0400

    Updated template package parser method to match other parsers.

--- packages/doxdox-parser-template/src/index.ts
+++ packages/doxdox-parser-template/src/index.ts
@@ -4,9 +4,19 @@ import { join } from 'path';
 
 import { File } from 'doxdox-core';
 
-export default async (cwd: string, path: string): Promise<File> =>
-    await parseString(path, await fs.readFile(join(cwd, path), 'utf8'));
+const parser = async (cwd: string, path: string): Promise<File> => {
+    try {
+        const content = await fs.readFile(join(cwd, path), 'utf8');
 
+        return await parseString(path, content);
+    } catch (err) {
+        if (process.env.DEBUG) {
+            console.error(err);
+        }
+    }
+
+    return { path, methods: [] };
+};
 export const parseString = async (
     path: string,
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -14,3 +24,5 @@ export const parseString = async (
 ): Promise<File> => {
     return { path, methods: [] };
 };
+
+export default parser;
