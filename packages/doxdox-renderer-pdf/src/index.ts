4c3070299f1d7db4aea74d1b2bf836d0caef7be1
Scott Doxey <neogeek0101@gmail.com>
2026-09-01 23:33:27 -0400

    Updated packages.

--- packages/doxdox-renderer-pdf/src/index.ts
+++ packages/doxdox-renderer-pdf/src/index.ts
@@ -172,8 +172,10 @@ export default async (doc: Doc): Promise<Buffer> => {
     const page = await browser.newPage();
 
     await page.setContent(html, {
-        waitUntil: 'networkidle0'
+        waitUntil: 'domcontentloaded'
     });
+
+    await page.waitForNetworkIdle({ idleTime: 500 });
 
     const pdf = await page.pdf({ format: 'A4' });
 
